1061e78fd4a64d2a60a47a5f1326d1e4a611cc87
constructionmccarthy-bit <constructionmccarthy@gmail.com>
2026-04-30 14:11:33 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-CMXyf3hABhbKlMMHmOv1MQN0zhLBDQQ",
+  authDomain: "findmypet-d9531.firebaseapp.com",
+  databaseURL: "https://findmypet-d9531-default-rtdb.firebaseio.com",
+  projectId: "findmypet-d9531",
+  storageBucket: "findmypet-d9531.firebasestorage.app",
+  messagingSenderId: "488618726667",
+  appId: "1:488618726667:web:a9aa39d663da904759be0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
